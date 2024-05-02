90f3e56c64d9cc192484000aaf368cb8312f8b50
Alexandr Kuz'min <kac13404@gmail.com>
2024-05-02 22:08:42 +0500

    введены исправления

--- ControlWork/Program.cs
+++ ControlWork/Program.cs
@@ -50,8 +50,7 @@ void printA(string[] stringArray)
     Console.Write("[");
     for (int i = 0; i < stringArray.Length; i++)
     {
-        Console.Write($""{ stringArray[i]}
-        "", ");
+        Console.Write($"'{stringArray[i]}', ");
     }
     Console.Write("]");
 }
